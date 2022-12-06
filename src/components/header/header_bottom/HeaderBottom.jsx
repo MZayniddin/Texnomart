24e88b50c4f9c0298baac42e6ef2dbe8c09bd55d
M.Zayniddin <93776172+MZayniddinov@users.noreply.github.com>
2022-12-06 05:56:55 +0500

    Header bottom ended

--- src/components/header/header_bottom/HeaderBottom.jsx
+++ src/components/header/header_bottom/HeaderBottom.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './HeaderBottom.scss'
+
+//icons 
+import { BsGrid } from 'react-icons/bs';
+import { IoClose } from 'react-icons/io5'
 
 const HeaderBottom = () => {
+  const [isCatalogActive, setIsCatalogActive] = useState(false);
+  const [categories, setCategories] = useState([])
+
+  useEffect(()=>{
+    fetch("https://api.escuelajs.co/api/v1/categories")
+    .then(response => response.json())
+    .then(data => setCategories(data))
+    .catch(error => console.log(error))
+  },[])
+  console.log(categories)
   return (
-    <div>HeaderBottom</div>
+    <div className='header-bottom'>
+      <button className='header-catalog-btn' onClick={()=>{setIsCatalogActive(!isCatalogActive)}}>
+        <span style={isCatalogActive ? {display: "none"} : {display: "block"}}><BsGrid className='catalog__icon'/></span>
+        <span style={isCatalogActive ? {display: "block"} : {display: "none"}}><IoClose className='catalog__icon'/></span>
+        <span className='catalog__text'>Katalog</span>
+      </button>
+      <ul className='header-bottom-list'>
+      {
+        categories.map(({id, name}) =>
+          <li key={id}>
+            <Link className='header-bottom__link' to="/">{name}</Link>
+          </li>  
+        )
+      }
+      </ul>
+    </div>
   )
 }
 
